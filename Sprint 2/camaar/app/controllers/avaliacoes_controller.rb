5d443c546019452c01fd2c69af7bdc914ea177ce
Gabriel <211056000@aluno.unb.br>
2026-06-15 11:32:14 -0300

    feat(#99,#109,#110,#113): AvaliacoesController - CRUD, responder, submeter e resultados
    - index: admin vê tudo; discente vê apenas formulários não respondidos de suas turmas
    - new/create: admin cria formulário com tipo (discente/docente)
    - responder/submeter: discente responde formulário com guard de duplicidade
    - resultados: admin visualiza respostas por questão

--- Sprint 2/camaar/app/controllers/avaliacoes_controller.rb
+++ Sprint 2/camaar/app/controllers/avaliacoes_controller.rb
@@ -1,22 +1,89 @@
 class AvaliacoesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin!, only: [:new, :create, :resultados]
+  before_action :set_avaliacao, only: [:show, :responder, :submeter, :resultados]
+  layout 'authenticated'
+
+  def index
+    @avaliacoes = if current_user.admin?
+      Avaliacao.includes(:template, :turma).all
+    else
+      turma_ids = current_user.turmas.pluck(:id)
+      Avaliacao.includes(:template, turma: :disciplina)
+               .para_discentes
+               .where(turma_id: turma_ids)
+               .nao_respondidas_por(current_user)
+    end
+  end
+
+  def show
+  end
+
   def new
     @avaliacao = Avaliacao.new
     @templates = Template.all
+    @turmas = Turma.includes(:disciplina).all
   end
 
   def create
     @avaliacao = Avaliacao.new(avaliacao_params)
     if @avaliacao.save
-      redirect_to avaliacoes_path, notice: "Formulário de avaliação disponibilizado para a turma."
+      redirect_to avaliacoes_path, notice: "Formulário disponibilizado para a turma."
     else
       @templates = Template.all
+      @turmas = Turma.includes(:disciplina).all
       render :new, status: :unprocessable_entity
     end
   end
 
+  def responder
+    @questoes = @avaliacao.template.questoes
+    redirect_to avaliacoes_path, alert: "Você já respondeu este formulário." if ja_respondeu?
+  end
+
+  def submeter
+    if ja_respondeu?
+      redirect_to avaliacoes_path, alert: "Você já respondeu este formulário."
+      return
+    end
+
+    respostas = build_respostas
+    if respostas.all?(&:valid?) && respostas.all?(&:save)
+      redirect_to avaliacoes_path, notice: "Respostas enviadas com sucesso."
+    else
+      @questoes = @avaliacao.template.questoes
+      flash.now[:alert] = "Preencha todas as questões."
+      render :responder, status: :unprocessable_entity
+    end
+  end
+
+  def resultados
+    @questoes = @avaliacao.template.questoes.includes(:respostas)
+    @total_respostas = Resposta.where(avaliacao: @avaliacao).select(:user_id).distinct.count
+  end
+
   private
 
+  def set_avaliacao
+    @avaliacao = Avaliacao.find(params[:id])
+  end
+
   def avaliacao_params
-    params.require(:avaliacao).permit(:template_id, :turma_id, :data_inicio, :data_fim)
+    params.require(:avaliacao).permit(:template_id, :turma_id, :data_inicio, :data_fim, :tipo)
+  end
+
+  def require_admin!
+    redirect_to dashboard_path, alert: "Acesso restrito." unless current_user.admin?
+  end
+
+  def ja_respondeu?
+    Resposta.exists?(user: current_user, avaliacao: @avaliacao)
+  end
+
+  def build_respostas
+    (params[:respostas] || {}).map do |questao_id, texto|
+      Resposta.new(user: current_user, avaliacao: @avaliacao,
+                   questao_id: questao_id, texto: texto)
+    end
   end
 end
